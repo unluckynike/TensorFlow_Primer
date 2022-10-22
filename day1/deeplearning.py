cc2230769b31b0b40405ddb2aff1a2587dc6fe7e
WuLiZeng <2230432084@qq.com>
2022-10-22 11:25:12 +0800

    环境🧑‍💻💻

---
+++ day1/deeplearning.py
@@ -0,0 +1,22 @@
+
+'''
+@Project ：TensorFlow_Primer 
+@File    ：deeplearning.py
+@Author  ：hailin
+@Date    ：2022/10/22 11:21 
+@Info    : 
+'''
+
+import tensorflow as tf
+
+def tensorflow_demo():
+    """
+    tensorflow 基本结构
+    :return:
+    """
+
+
+    return None
+
+if __name__ == '__main__':
+    print(tf.__version__)
