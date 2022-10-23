c555327e7d6b7b5989cc0214e34669195e4bf2b5
WuLiZeng <2230432084@qq.com>
2022-10-23 23:06:09 +0800

    二进制文件读取

---
+++ day2/deeplearning_binary.py
@@ -0,0 +1,20 @@
+
+'''
+@Project ：TensorFlow_Primer 
+@File    ：deeplearning_binary.py
+@Author  ：hailin
+@Date    ：2022/10/23 22:59 
+@Info    : CIFAR10  读取二进制文件
+'''
+import tensorflow.compat.v1 as tf
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def read_binary():
+
+    return None
+
+if __name__ == '__main__':
+    print(tf.__version__)
+    tf.compat.v1.disable_eager_execution()
