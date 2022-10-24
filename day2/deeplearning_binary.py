53b3d08efac7f8d9399024d2666a00d1e6d1bd45
WuLiZeng <2230432084@qq.com>
2022-10-24 16:32:54 +0800

    tfrecorder

--- day2/deeplearning_binary.py
+++ day2/deeplearning_binary.py
@@ -100,6 +100,58 @@ class Cifar:
 
         return None
 
+    def read_tfrecords(self):
+        """
+        读取TFRecords文件
+        :return:
+        """
+        # 1、构造文件名队列
+        file_queue = tf.train.string_input_producer(["cifar10.tfrecords"])
+
+        # 2、读取与解码
+        # 读取
+        reader = tf.TFRecordReader()
+        key, value = reader.read(file_queue)
+
+        # 解析example
+        feature = tf.parse_single_example(value, features={
+            "image": tf.FixedLenFeature([], tf.string),
+            "label": tf.FixedLenFeature([], tf.int64)
+        })
+        image = feature["image"]
+        label = feature["label"]
+        print("read_tf_image:\n", image)
+        print("read_tf_label:\n", label)
+
+        # 解码
+        image_decoded = tf.decode_raw(image, tf.uint8)
+        print("image_decoded:\n", image_decoded)
+        # 图像形状调整
+        image_reshaped = tf.reshape(image_decoded, [self.height, self.width, self.channel])
+        print("image_reshaped:\n", image_reshaped)
+
+        # 3、构造批处理队列
+        image_batch, label_batch = tf.train.batch([image_reshaped, label], batch_size=100, num_threads=2, capacity=100)
+        print("image_batch:\n", image_batch)
+        print("label_batch:\n", label_batch)
+
+        # 开启会话
+        with tf.Session() as sess:
+
+            # 开启线程
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+            image_value, label_value = sess.run([image_batch, label_batch])
+            print("image_value:\n", image_value)
+            print("label_value:\n", label_value)
+
+            # 回收资源
+            coord.request_stop()
+            coord.join(threads)
+
+        return None
+
 
 if __name__ == '__main__':
     print(tf.__version__)
